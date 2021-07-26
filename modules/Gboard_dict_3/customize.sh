4866a89a2e3c0b7d4b66154ab99ecc4f4a50b383
entr0pia <30486766+entr0pia@users.noreply.github.com>
2021-07-26 09:52:49 +0800

    update gboard-dict-3 script

--- modules/Gboard_dict_3/customize.sh
+++ modules/Gboard_dict_3/customize.sh
@@ -1,8 +1,11 @@
 # 注意 这不是占位符！！这个代码的作用是将模块里的东西全部塞系统里，然后挂上默认权限
 SKIPUNZIP=0
 
-uid=$(dumpsys package com.google.android.inputmethod.latin | grep  userId | head -n 1 | sed 's/.*userId=\([0-9]*\).*/\1/g')
-uid=u0_a${uid:1}
+uid=$(ls -n /data/data | grep "com.google.android.inputmethod.latin" | awk '{print $3}')
+if [ -z "$uid" ]; then
+    abort "请检查是否安装 Gboard"
+    abort "退出安装"
+fi
 mv /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3 /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3.bak
 cp -f $MODPATH/user_dict_3_3 /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3
 chown $uid /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3
